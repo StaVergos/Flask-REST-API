99e199e9c56bfe92f05ebe28aee5391a936e16bf
Stavros Vergos <stavrospvergos@gmail.com>
2020-05-06 21:30:00 +0300

    added item and store with marshmallow

--- app.py
+++ app.py
@@ -1,11 +1,14 @@
-from flask import Flask
+from flask import Flask, jsonify
 from flask_restful import Api
 from flask_jwt_extended import JWTManager
+from marshmallow import ValidationError
 
 from blacklist import BLACKLIST
 from ma import ma
 
 from resources.user import UserRegister, User, UserLogin, TokenRefresh, UserLogout
+from resources.item import Item, ItemList
+from resources.store import Store, StoreList
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
@@ -19,9 +22,14 @@ app.config['JWT_BLACKLIST_TOKEN_CHECKS'] = [
 app.secret_key = 'kalamitis' #app.config['JWT_SECRET_KEY']
 api = Api(app)
 
+
 @app.before_first_request
 def create_tables():
     db.create_all()
+
+@app.errorhandler(ValidationError)
+def handle_marshmallow_validation(err):
+    return jsonify(err.messages), 400
 
 
 jwt = JWTManager(app)
@@ -36,6 +44,10 @@ api.add_resource(UserRegister, '/register')
 api.add_resource(User, '/user/<int:user_id>')
 api.add_resource(UserLogin, '/login')
 api.add_resource(UserLogout, '/logout')
+api.add_resource(Item, '/item/<string:name>')
+api.add_resource(ItemList, '/items')
+api.add_resource(Store, '/store/<string:name>')
+api.add_resource(StoreList, '/stores')
 api.add_resource(TokenRefresh, '/refresh')
 
 if __name__ == '__main__':
